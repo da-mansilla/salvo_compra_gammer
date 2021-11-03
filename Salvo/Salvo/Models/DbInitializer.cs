2d4bfa0a2587ab4b18d675896b5836d13c6d239a
Agustin Mansilla <mansillaagustin1019@gmail.com>
2021-11-03 18:00:56 -0300

    Añadido GamesCOntroller

---
+++ Salvo/Salvo/Models/DbInitializer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Salvo.Models
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(SalvoContext context)
+        {
+            if (!context.Players.Any())
+            {
+                var players = new Player[]
+                {
+                    new Player
+                    {
+                        Email = "j.bauer@ctu.gov",
+                        Name = "Jack Bauer",
+                        Password = "24"
+                    },
+                    new Player
+                    {
+                        Email = "c.obrain@ctu.gov",
+                        Name = "Chloe O'Brian",
+                        Password = "42"
+                    },
+                    new Player
+                    {
+                        Email = "kim_bauer@gmail.com",
+                        Name = "Kim Bauer",
+                        Password = "kb"
+                    },
+                    new Player
+                    {
+                        Email = "t.almeida@ctu.gov",
+                        Name = "Tony Almeida",
+                        Password = "mole"
+                    }
+                };
+                // Recorremos los players
+                foreach(Player player in players)
+                {
+                    context.Players.Add(player);
+                }
+                // Guardamos los cambios
+                context.SaveChanges();
+
+            }
+            if (!context.Games.Any())
+            {
+                var games = new Game[]
+                {
+                    new Game {CreationDate = DateTime.Now },
+                    new Game {CreationDate = DateTime.Now.AddHours(1) },
+                    new Game {CreationDate = DateTime.Now.AddHours(2) },
+                    new Game {CreationDate = DateTime.Now.AddHours(3) },
+                    new Game {CreationDate = DateTime.Now.AddHours(4) },
+                    new Game {CreationDate = DateTime.Now.AddHours(5) },
+                    new Game {CreationDate = DateTime.Now.AddHours(6) },
+                    new Game {CreationDate = DateTime.Now.AddHours(7) }
+                };
+                // Recorremos los games
+                foreach (Game game in games)
+                {
+                    context.Games.Add(game);
+                }
+                // Guardamos los cambios
+                context.SaveChanges();
+            }
+            
+        }
+    }
+}
---
+++ Salvo/Salvo/Models/SalvoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Salvo.Models
+{
+    public class SalvoContext : DbContext
+    {
+        public SalvoContext(DbContextOptions<SalvoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+    }
+}
